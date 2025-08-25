1938439cd9e7b3345cf88b7e228806411f7cac4d
Leticia <segurasseleticia@gmail.com>
2025-08-25 19:16:58 -0300

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,9 +30,6 @@ const Footer = () => {
               <a href="#" className="text-neutral-300 hover:text-white transition-colors">
                 <Instagram size={20} />
               </a>
-              <a href="#" className="text-neutral-300 hover:text-white transition-colors">
-                <Linkedin size={20} />
-              </a>
             </div>
           </div>
 
